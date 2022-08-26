5794e9ad2d35b0a536e6f8078204a7b9f2430a8a
Heron <jr_heronildes@hotmail.com>
2022-08-26 14:09:34 -0300

    initial commit

---
+++ dist/js/scripts.js
@@ -0,0 +1,11 @@
+/*!
+* Start Bootstrap - SIGMun Hero Package v0.0.1 (undefined)
+* Copyright 2013-2022 Heronildes Junior
+* Licensed under MIT (https://github.com/heronildesjr/sigmun-hero/blob/master/LICENSE)
+*/
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+
+
+
