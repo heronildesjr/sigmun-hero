fabd6133d461f410d6346590c942585de668667d
Heron <jr_heronildes@hotmail.com>
2022-08-31 10:22:12 -0300

    melhorias ui

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -2,5 +2,8 @@
 // Use this file to add JavaScript to your project
 
 
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl));
 
-
+const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
+const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
