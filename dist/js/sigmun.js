d1c3a3f4abc39bd120a80447bc4995609faadabb
Heron <jr_heronildes@hotmail.com>
2023-05-18 10:00:34 -0300

    melhorias visuais e organizacionais

--- dist/js/sigmun.js
+++ dist/js/sigmun.js
@@ -16,8 +16,55 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl), {boundary: document.body});
 
-jQuery(window).on('resize', function() {
-    $('.sm-hero__header-title').css('margin-top', jQuery('#main-navbar').outerHeight() + 3 + 'px');
-});
-$('.sm-hero__header-title').css('margin-top', jQuery('#main-navbar').outerHeight() + 3 + 'px');
+// jQuery(window).on('resize', function() {
+//     $('.sm-hero__header-title').css('margin-top', jQuery('#main-navbar').outerHeight() + 3 + 'px');
+// });
+// $('.sm-hero__header-title').css('margin-top', jQuery('#main-navbar').outerHeight() + 3 + 'px');
+
+
+// prefixo objeto sigmun
+const sigmun = {
+    // funcoes
+    functions: {
+        // alerts
+        showAlert: (message, type, container, timeout) => {
+            /* types disponiveis: primary, secondary, success, danger, warning, info, light, dark */
+            if (!container) {
+                container = '#alert-container'
+            }
+
+            if (!type) {
+                type = 'primary'
+            }
+
+            if (!timeout) {
+                timeout = 0
+            }
+
+            const containerWrapper = document.querySelector(container);
+
+            const wrapper = document.createElement('div');
+            wrapper.innerHTML = [
+            `<div class="alert alert-${type} alert-dismissible fade show fade-in" role="alert">`,
+            `   <div>${message}</div>`,
+            '   <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>',
+            '</div>'
+            ].join('');
+
+            containerWrapper.append(wrapper);
+
+            if (timeout > 0) {
+                setTimeout(() => {
+                    wrapper.classList.add('fade-out');
+                    setTimeout(() => wrapper.remove(), 200);
+                }, timeout);
+            }
+        },
+
+        // adiuadwhuiawdhuiawdhuis
+
+    }
+
+};
+
 
